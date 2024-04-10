5fda7c63edbed6acda5670a41fec0d1d0d55cbc1
Mohsen Pakfetrat <mohsenpk1370@gmail.com>
2024-04-10 11:44:54 +0330

    feat: add i18n

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<script setup>
+const { locale, setLocale } = useI18n();
+</script>
+
+<template>
+  <div>
+    <div class="p-8 flex gap-8">
+      <button 
+        class="p-3 rounded-lg border"
+        :class="locale === 'en' ? 'bg-blue-300' : 'bg-white'" 
+        @click="setLocale('en')">
+          English
+        </button>
+      <button 
+        class="p-3 rounded-lg border" 
+        :class="locale === 'fr' ? 'bg-blue-300' : 'bg-white'" 
+        @click="setLocale('fr')">
+          French
+        </button>
+    </div>
+    <p class="text-xl text-center py-44">{{ $t('welcome') }}</p>
+  </div>
+</template>
